8f4cd93ff87a84f460974b0783efa052435c340e
13geethika <geethika.bodapati@gmail.com>
2026-06-20 23:06:06 +0530

    Changed url

--- frontend/src/shared/services/api.ts
+++ frontend/src/shared/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://scheduled-messaging-platform.onrender.com";//'http://localhost:8080/api' ||
 
 
 const api = axios.create({
